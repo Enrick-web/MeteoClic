7cbe8f65f640ffb6c80661f8d30e85bec857c4b2
Enrick Gascono <58599224+Enrick-web@users.noreply.github.com>
2020-10-12 23:59:03 +0200

    Add gitignore et ajout de l'heure
    Ajout d'un script afin d'ajouter l'heure actuelle, modification du Titre et du fond d'écran

--- meteo.js
+++ meteo.js
@@ -43,8 +43,17 @@ document.querySelector("form").addEventListener("submit", function (e) {
 apiCall("Paris");
 
 // Heure
-let today = new Date;
-let heure = document.querySelector("#clock");
-document.querySelector('#clock').innerHTML = today.toString;
+function getTime() {
+  var date = new Date();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+  hours = ((hours < 10) ? " 0" : " ") + hours;
+  minutes = ((minutes < 10) ? ":0" : ":") + minutes;
+  seconds = ((seconds < 10) ? ":0" : ":") + seconds;
+  var myHour = document.getElementById("my-hour");
+  myHour.textContent = hours + minutes + seconds;
+  setTimeout("getTime()", 1000);
 
-console.log(today.toString())
+}
+getTime();
